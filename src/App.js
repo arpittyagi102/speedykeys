1a358118c5bb47dc8bbe3a3ab4dd271ebf4fb14b
Arpit <arpittyagirocks@gmail.com>
2023-06-03 00:19:47 +0530

    added WPM and accuracy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [typingstatus,settypingstatus]=useState(true);
-  const string="a quick brown fox jumps over the lazy dog";
+  const [wrong,setwrong]=useState(false);
+  const [inputvalue, setinputvalue]=useState("");
+  const [starttime,setstarttime]=useState(0);
+  const [timetaken,settimetaken]=useState(0);
+  const [mistakes,setmistakes]=useState(0);
+  var string="a quick brown fox jumps over the lazy dog";  
+  string="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
   function handleinputchange(e){
+    if(starttime===0)
+      setstarttime(Date.now());
+    if(string===e.target.value)
+      completed();
     if(!string.startsWith(e.target.value)){
-      settypingstatus(false);
+      setwrong(true);
+      setTimeout(() => setwrong(false), 500);
+      setmistakes(mistakes+1);
+      return;
     }
-    else{
-      settypingstatus(true);
-    }
+    setinputvalue(e.target.value);
   }
+
+  function completed(){
+    var endtime=Date.now();
+    settimetaken((endtime-starttime)/1000);
+  }
+
   return (
     <>
       <div className='outer'>
         <h1 style={{textAlign:"center"}}>Speedy Keys</h1>
-        <div className='question-lines'>
-          a quick brown fox jumps over the lazy dog
+        <div className={`question ${wrong?'wrong':''}`}>
+          {string}
         </div>
-        <div className={`input-outer ${typingstatus?"true":"false"}`}>
-          <input className="main-input"  onChange={handleinputchange}/>
+        <input className='answer' 
+              value={inputvalue} 
+              onChange={handleinputchange}/>
+        <div>
+          Your WPM = {Math.round((string.length*12)/timetaken)}
+        </div>
+        <div>
+          Your accuracy = {Math.round(((string.length-mistakes)/string.length)*100)}
         </div>
       </div>
     </>
