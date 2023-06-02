05741b94d2063485e4ef20071184623f530ae602
Arpit <arpittyagirocks@gmail.com>
2023-06-02 15:27:14 +0530

    spell checking is working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [typingstatus,settypingstatus]=useState(true);
+  const string="a quick brown fox jumps over the lazy dog";
+  function handleinputchange(e){
+    if(!string.startsWith(e.target.value)){
+      settypingstatus(false);
+    }
+    else{
+      settypingstatus(true);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='outer'>
+        <h1 style={{textAlign:"center"}}>Speedy Keys</h1>
+        <div className='question-lines'>
+          a quick brown fox jumps over the lazy dog
+        </div>
+        <div className={`input-outer ${typingstatus?"true":"false"}`}>
+          <input className="main-input"  onChange={handleinputchange}/>
+        </div>
+      </div>
+    </>
   );
 }
 
