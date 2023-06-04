1e0e43fa0f2e81ca0317b4b83ead61443bcd3b23
Arpit <arpittyagirocks@gmail.com>
2023-06-04 17:09:14 +0530

    Added time limit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import keyboard from './Asset/keyboard-image.png';
 import data from './Asset/data.json';
@@ -6,18 +6,30 @@ import data from './Asset/data.json';
 function App() {
   const [wrong,setwrong]=useState(false);
   const [inputvalue, setinputvalue]=useState("");
+  const [timeinput,settimeinput]=useState(30);
   const [starttime,setstarttime]=useState(0);
   const [timetaken,settimetaken]=useState(0);
   const [correct,setcorrect]=useState(0)
   const [incorrect,setincorrect]=useState(0);
-  var string=data[0].text;  
-  
+  const [i,seti]=useState(0);             
+  const [string,setstring]=useState(data[i].text)
 
   function handleinputchange(e){
-    if(starttime===0)
+    if(starttime===0){
       setstarttime(Date.now());
-    if(string===e.target.value)
-      completed();
+      start();
+    }
+    if(string===e.target.value){
+      setinputvalue("");
+      console.log("Now we will change the i =",i);
+      seti(i+1);
+      console.log("Changed value of i is ,",i);
+      setstring(data[i].text);
+      console.log("Now the changed string is ",string);
+      return;
+    }
+    if(e.target.value.length===49)
+      setinputvalue(inputvalue+" ")
     if(!string.startsWith(e.target.value)){
       setwrong(true);
       setTimeout(() => setwrong(false), 500);
@@ -26,6 +38,19 @@ function App() {
     }
     setinputvalue(e.target.value);
     setcorrect(correct+1);
+  }
+
+
+  useEffect(() => {
+    if (i < data.length) {
+      setstring(data[i].text);
+    }
+  }, [i]);
+
+  function start(){
+    setTimeout(() => {
+      completed();
+    }, timeinput*1000);
   }
 
   function completed(){
@@ -37,23 +62,36 @@ function App() {
     <>
       <div className='outer'>
         <h1 style={{textAlign:"center"}}>Speedy Keys</h1>
+        <div className='dropdowns'>
+          <select>
+            <option>Basic</option>
+            <option>Intermediate</option>
+            <option>Advanced</option>
+          </select>
+          <select value={timeinput} onChange={(e)=>{settimeinput(e.target.value)}}>
+            <option value="30">00:30</option>
+            <option value="60">01:00</option>
+            <option value="120">02:00</option>
+            <option value="300">05:00</option>
+          </select>
+        </div>
         <div className={`question ${wrong?'wrong':''}`}>
           {string}
         </div>
-        <input className='answer' 
+        <textarea className='answer' 
               value={inputvalue} 
               onChange={handleinputchange}/>
         <div className={`result  ${timetaken===0?"d-none":""}`}>
           <div className='wpm'>
-            {Math.round((correct*12)/timetaken)} wpm
+            {Math.round((correct*12)/timeinput)} wpm
           </div>
           <div className='accuracy box-down'>
             <div className='parameter'>Gross Speed</div> 
-            <div className="value">{Math.round(((correct+incorrect)*12)/timetaken)}</div> 
+            <div className="value">{Math.round(((correct+incorrect)*12)/timeinput)}</div> 
           </div>
           <div className='accuracy box-down'>
             <div className='parameter'>Accuracy</div> 
-            <div className="value">{Math.round(((incorrect)/correct)*100)}%</div> 
+            <div className="value">{Math.round(((correct)/(incorrect+correct))*100)}%</div> 
           </div>
           <div className='correct box-down'>
             <div className='parameter'>Correct Characters</div> 
@@ -65,7 +103,7 @@ function App() {
           </div>
           <div className="time box-down">
             <div className='parameter'>Time Taken</div> 
-            <div className="value">{timetaken}</div>
+            <div className="value">{Math.round(timeinput)} s</div>
           </div>
         </div>
         <img src={keyboard} alt='keyboard' className={` keyboard image ${timetaken===0?"":"d-none"}`}/>
