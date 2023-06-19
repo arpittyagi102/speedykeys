add954063be9e2c7d88e24240dc746450eb5c84b
Arpit <arpittyagirocks@gmail.com>
2023-06-19 16:34:35 +0530

    Trying to fix the errro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import keyboard from './Asset/keyboard-image.png';
 import data from './Asset/data.json';
@@ -34,6 +34,7 @@ function App() {
     settimetaken(0);
     setcorrect(0);
     setincorrect(0);
+    console.log(`I is now changed to ${i}`);
     setstring(data[e.target.value].text);
   }
   
@@ -46,7 +47,7 @@ function App() {
     if(string===e.target.value){
       setinputvalue("");
       console.log("Now we will change the i =",i);
-      seti(i+1);
+      seti(prevIndex => prevIndex+1);
       console.log("Changed value of i is ,",i);
       setstring(data[i].text);
       console.log("Now the changed string is ",string);
@@ -66,7 +67,8 @@ function App() {
 
   useEffect(() => {
     if (i < data.length) {
-      setstring(data[i].text);
+      if(data[i])
+        setstring(data[i].text);
     }
   }, [i]);
 
@@ -152,7 +154,7 @@ function App() {
           </div>
           <div className='refresh box-down'>
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0,0,256,256" className='refresh-icon' onClick={restart}>
-              <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{mixBlendMode: "normal"}}><g transform="scale(5.12,5.12)"><path d="M25,2c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175c10.51712,0 19,8.48288 19,19c0,10.51712 -8.48288,19 -19,19c-10.51712,0 -19,-8.48288 -19,-19c0,-5.4758 2.30802,-10.39189 6,-13.85547v3.85547c-0.0102,0.72127 0.36875,1.39216 0.99175,1.75578c0.623,0.36361 1.39351,0.36361 2.01651,0c0.623,-0.36361 1.00195,-1.0345 0.99175,-1.75578v-11h-11c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h4.52539c-4.61869,4.20948 -7.52539,10.27232 -7.52539,17c0,12.67888 10.32112,23 23,23c12.67888,0 23,-10.32112 23,-23c0,-12.67888 -10.32112,-23 -23,-23z"></path></g></g>
+              <g fill="#ffffff" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{mixBlendMode: "normal"}}><g transform="scale(5.12,5.12)"><path d="M25,2c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175c10.51712,0 19,8.48288 19,19c0,10.51712 -8.48288,19 -19,19c-10.51712,0 -19,-8.48288 -19,-19c0,-5.4758 2.30802,-10.39189 6,-13.85547v3.85547c-0.0102,0.72127 0.36875,1.39216 0.99175,1.75578c0.623,0.36361 1.39351,0.36361 2.01651,0c0.623,-0.36361 1.00195,-1.0345 0.99175,-1.75578v-11h-11c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h4.52539c-4.61869,4.20948 -7.52539,10.27232 -7.52539,17c0,12.67888 10.32112,23 23,23c12.67888,0 23,-10.32112 23,-23c0,-12.67888 -10.32112,-23 -23,-23z"></path></g></g>
             </svg>
           </div>
         </div>
