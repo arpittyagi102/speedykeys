cecad6968739d4a4d1d208d1ffdddaa64e190996
Arpit <arpittyagirocks@gmail.com>
2023-06-19 15:52:31 +0530

    Added the restart button at the end

--- src/App.js
+++ src/App.js
@@ -16,7 +16,17 @@ function App() {
   const [timeleft,settimeleft]=useState(timeinput);
   const [diffinput,setdiffinput]=useState(18);
 
-  function restart(e){
+  function restart(){
+    setinputvalue("");
+    setstarttime(0);
+    settimetaken(0);
+    setcorrect(0);
+    setincorrect(0);
+    seti(diffinput);
+    setstring(data[i].text);
+  }
+  
+  function restartbydiffinput(e){
     setdiffinput(e.target.value);
     seti(e.target.value);
     setinputvalue("");
@@ -24,7 +34,7 @@ function App() {
     settimetaken(0);
     setcorrect(0);
     setincorrect(0);
-    setstring(data[i].text);
+    setstring(data[e.target.value].text);
   }
   
   function handleinputchange(e){
@@ -96,7 +106,7 @@ function App() {
           <h1 style={{textAlign:"center"}}>{timeleft}</h1>
         )}
         <div className='dropdowns'>
-          <select value={diffinput} onChange={restart}>
+          <select value={diffinput} onChange={restartbydiffinput}>
             <option value={0}>Basic</option>
             <option value={18}>Intermediate</option>
             <option value={30}>Advanced</option>
@@ -116,7 +126,7 @@ function App() {
               onChange={handleinputchange}
               autoFocus
               />
-        <div className={`result  ${timetaken===0 && "d-none"}`}>
+        <div className={`result  ${ timetaken===0 && "d-none"}`}>
           <div className='wpm'>
             {Math.round((correct*12)/timeinput)} wpm
           </div>
@@ -139,6 +149,11 @@ function App() {
           <div className="time box-down">
             <div className='parameter'>Time Taken</div> 
             <div className="value">{Math.round(timeinput)} s</div>
+          </div>
+          <div className='refresh box-down'>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0,0,256,256" className='refresh-icon' onClick={restart}>
+              <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{mixBlendMode: "normal"}}><g transform="scale(5.12,5.12)"><path d="M25,2c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175c10.51712,0 19,8.48288 19,19c0,10.51712 -8.48288,19 -19,19c-10.51712,0 -19,-8.48288 -19,-19c0,-5.4758 2.30802,-10.39189 6,-13.85547v3.85547c-0.0102,0.72127 0.36875,1.39216 0.99175,1.75578c0.623,0.36361 1.39351,0.36361 2.01651,0c0.623,-0.36361 1.00195,-1.0345 0.99175,-1.75578v-11h-11c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h4.52539c-4.61869,4.20948 -7.52539,10.27232 -7.52539,17c0,12.67888 10.32112,23 23,23c12.67888,0 23,-10.32112 23,-23c0,-12.67888 -10.32112,-23 -23,-23z"></path></g></g>
+            </svg>
           </div>
         </div>
         <img src={keyboard} alt='keyboard' className={`keyboard image ${timetaken!==0 && "d-none"}`}/>
